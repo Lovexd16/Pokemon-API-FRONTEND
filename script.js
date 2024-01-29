c9fee60a8d38f8727c81f1d73dc4589adfb8fb05
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-01-29 13:06:29 +0100

    Snygg design och pokemon visas på bra sätt

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 console.log("test");
 
 let pokemonList = document.getElementById("pokemonList");
+let promises = [];
 
-fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
-.then(res => res.json())
-.then(data => {
-    
-    printPokemonList(data);
+for (let i = 1; i <= 151; i++) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url)
+    .then(res => res.json()));
+};
 
+Promise.all(promises).then(results => {
+    let pokemon = results.map(data => ({
+        pokemonName: data.name,
+        pokemonId: data.id,
+        pokemonImg: data.sprites['front_default'],
+        type: data.types.map((type) => type.type.name).join(", ")
+    }));
+    printPokemonList(pokemon)
 });
 
-function printPokemonList(pokemons) {
-    pokemons.results.forEach(pokemon => {
-        console.log("pokemon", pokemon);
+function printPokemonList(pokemon) {
+    console.log(pokemon);
 
-    let pokemonName = document.createElement("p");
-    pokemonName.innerText = pokemon.name;
-    pokemonList.appendChild(pokemonName);
-    })
-}
+    pokemon.forEach(pokemonData => {
+        let pokemonDiv = document.createElement("div");
+        pokemonDiv.classList.add("pokemon");
+
+        let pokemonName = document.createElement("h2");
+        pokemonName.innerText = pokemonData.pokemonId + ". " +  pokemonData.pokemonName;
+
+        let pokemonImg = document.createElement("img");
+        pokemonImg.src = pokemonData.pokemonImg;
+
+        let pokemonTypes = document.createElement("p");
+        pokemonTypes.innerText = "Typer: " + pokemonData.type;
+        
+        pokemonDiv.append(pokemonName, pokemonImg, pokemonTypes);
+        pokemonList.appendChild(pokemonDiv);
+    });
+    
+};
