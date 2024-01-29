3bd472f40515f33bd10b53ce97412d8f373a0ce8
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-01-29 10:44:24 +0100

    Jag får nu ut pokemon från pokeapi på min sida

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("test");
+
+let pokemonList = document.getElementById("pokemonList");
+
+fetch("https://pokeapi.co/api/v2/pokemon?limit=30")
+.then(res => res.json())
+.then(data => {
+    
+    printPokemonList(data);
+
+});
+
+function printPokemonList(pokemons) {
+    pokemons.results.forEach(pokemon => {
+        console.log("pokemon", pokemon);
+
+    let pokemonName = document.createElement("p");
+    pokemonName.innerText = pokemon.name;
+    pokemonList.appendChild(pokemonName);
+    })
+}
